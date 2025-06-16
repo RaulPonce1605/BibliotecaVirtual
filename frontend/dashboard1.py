1ecc3ac3e6070821a9355ec910b3b994d2e486db
RaulPonce <josericoponce36@gmail.com>
2025-06-15 18:04:20 -0600

    Implementacion del registro material

--- frontend/dashboard1.py
+++ frontend/dashboard1.py
@@ -43,7 +43,7 @@ class DashboardWindow(QMainWindow):
     def setup_connections(self):
         # Botones de navegación
         self.ui.bt_uno.clicked.connect(self.abrir_registro_usuario)
-        self.ui.bt_dos.clicked.connect(lambda: self.cambiar_pagina(1))
+        self.ui.bt_dos.clicked.connect(self.abrir_registro_material)
         self.ui.bt_tres.clicked.connect(lambda: self.cambiar_pagina(2))
         self.ui.bt_cuatro.clicked.connect(lambda: self.cambiar_pagina(3))
 
@@ -103,5 +103,11 @@ class DashboardWindow(QMainWindow):
     def abrir_registro_usuario(self):
         from registro_usuario import RegistroApp
         self.registro_window = RegistroApp(dashboard_window=self)
+        self.hide()
+        self.registro_window.show()
+
+    def abrir_registro_material(self):
+        from registro_material import RegistroMaterialApp
+        self.registro_window = RegistroMaterialApp(dashboard_window=self)
         self.hide()
         self.registro_window.show()
